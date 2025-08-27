97ee4a44a74399657b0fd3e08ae7343238cf84e6
FireFighter17 <souhaylsouhayl2007@gmail.com>
2025-08-27 10:35:15 +0100

    Updated website content

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ document.addEventListener('DOMContentLoaded',function(){
     });
   }
 
-  const answers = {q1:'Triassic', q2:'Theropod', q3:'true'};
+  const answers = {q1:'Triassic', q2:'Theropod', q3:'true', q4: 'Titanosaurus', q5: 'Stegosaurus', q6: 'Therizinosaurus', q7: 'Cretaceous', q8: 'Spinosaurus', q9: 'Pterodactyl', q10: 'Triceratops'};
   const checkBtn = document.getElementById('check');
   const scoreP = document.getElementById('score');
   if(checkBtn){
     checkBtn.addEventListener('click',function(){
-      let score = 0; let total = 3;
+      let score = 0; let total = 10;
       for(const key in answers){
         const selected = document.querySelector(`input[name="${key}"]:checked`);
         if(selected && selected.value === answers[key]) score++;
